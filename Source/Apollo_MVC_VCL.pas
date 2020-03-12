0b8bd402d44f56f4aa6d2368de192dfdf24993de
alezzle <niekras@tut.by>
2020-03-12 15:17:56 +0300

    dev

--- Source/Apollo_MVC_VCL.pas
+++ Source/Apollo_MVC_VCL.pas
@@ -4,12 +4,14 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs,
+  Apollo_MVC_Core;
 
 type
   TViewVCLBase = class abstract(TForm)
   private
     { Private declarations }
+    FView: TView;
   public
     { Public declarations }
   end;
